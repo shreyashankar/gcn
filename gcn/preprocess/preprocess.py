b8e64df8d7e856091f4ca831a6dda988892f879e
avgupta <av1659@gmail.com>
2017-05-29 17:54:04 -0700

    almost done preprocess

--- gcn/preprocess/preprocess.py
+++ gcn/preprocess/preprocess.py
@@ -1,44 +1,65 @@
 import json
+import numpy as np
 import argparse
+from sklearn.preprocessing import LabelEncoder
+
+def one_hot_encode(all_features):
+	le = LabelEncoder()
+	for col in range(all_features.shape[1]):
+		try:
+			all_features[:,col] = all_features[:,col].astype(float)
+		except ValueError:
+			print "Not a float"
+			print all_features[0,col]
+			all_features[:,col] = le.fit_transform(all_features[:,col])
+	return all_features
 
 def getNodeFeatures(rawFile):
 	"""
 		Generate the N*F feature file and N*E output files
 	"""
-	json_object = json.load(rawFile)
+	with open(rawFile) as data_file:    
+		json_object = json.load(data_file)
 
-	num_features = 1
-	for element in json_object[0]:
-		if type(element) is list or type(element) is dict:
-			num_features += len(element)
-		else:
-			num_features += 1
+	feature_array = []
 
-	feature_array = np.empty([len(json_object), num_features])
-
-	i = 0
-	for node, node_list in json_object:
-		feature_array[i,0] = node
-		k = 0
-		for element in node_list:
-			if type(element) is list or type(element) is dict:
-				for subelement in element:
-					feature_array[i, k] = subelement
-					k+=1
+	counter = 0
+	for node, node_list in json_object.iteritems():
+		node_array = [node]
+		if(counter == 0):
+			key_array = [node]
+		for element_key, element_val in node_list.iteritems():
+			if type(element_val) is list or type(element_val) is dict:
+				for subelement_key, subelement_val in element_val.iteritems():
+					node_array += [subelement_val]
+					if(counter == 0):
+						key_array += [element_key + "." + subelement_key]
 			else:
-				feature_array[i, k] = element
-				k+=1
-		i+=1
+				node_array += [element_val]
+				if(counter == 0): 
+					key_array += [element_key]
+		feature_array.append([node_array])
+		counter += 1
 
-	return feature_array
+	df = np.asmatrix(np.concatenate(feature_array), dtype='O')
+	print df.shape
+	class_index = key_array.index('Class')
+	print class_index
+	df_classes = np.column_stack([df[:,0], df[:,class_index]])
+	print df_classes.shape
+	df_feat = np.array(df[-class_index])
+
+	df_out_classes = one_hot_encode(df_classes)
+	df_out_feat = one_hot_encode(df_feat)
+
+	return df_out_classes, df_out_feat
 
 def main():
-	parser = argparse.ArgumentParser(description='Preprocess neural data')
-	parser.add_argument('-n','--node_json', help='Node json file',required=True)
-	args = parser.parse_args()
-	getNodeFeatures(args.node_json)
+	getNodeFeatures("neuronsinfo.json")
 
 
+if __name__ == '__main__':
+    main()
 
 
 
