4d362e2e9739b72da90d62dcfbc77839d43db7e0
Shreya Shankar <shreya1@stanford.edu>
2017-06-13 15:36:28 -0700

    changing model

--- gcn/models.py
+++ gcn/models.py
@@ -162,16 +162,16 @@ class GCN_Hybrid(Model):
 
     def _build(self):
 
-        self.layers.append(Dense(input_dim=FLAGS.input_dim,
-                                output_dim=self.output_dim,
+        self.layers.append(Dense(input_dim=self.input_dim,
+                                output_dim=FLAGS.hidden1,
                                 placeholders=self.placeholders,
                                 act=tf.nn.relu,
                                 dropout=True,
                                 sparse_inputs = True,
                                 logging=self.logging))
 
-        self.layers.append(GraphConvolution(input_dim=self.input_dim,
-                                            output_dim=FLAGS.hidden1,
+        self.layers.append(GraphConvolution(input_dim=FLAGS.hidden1,
+                                            output_dim=self.output_dim,
                                             placeholders=self.placeholders,
                                             act=lambda x: x,
                                             dropout=True,
